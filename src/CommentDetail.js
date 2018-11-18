010ca5645749a4f4b5816aa9eddd8c3128e52fa2
felix-csm <felixcsm@gmail.com>
2018-11-18 23:00:38 +0800

    last update

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import faker from 'faker';
 
 const CommentDetail = props => {
+    console.log(props);
     return (
         <div className="comment">
             <a href="./" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()} />
+                <img alt="avatar" src={props.avatar}/>
             </a>
             <div className="content">
             <a href="/" className="author">{props.author}</a>
             <div className="metadata">
-                <span className="date">Today at 5:42PM</span>
+                <span className="date">{props.timeAgo}</span>
             </div>
             <div className="text">
-                How artistic!
-            </div>
-            <div className="actions">
-                <a href="/" className="reply">Reply</a>
+                {props.content}
             </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
             <h3 className="ui dividing header">Comments</h3>
-            <CommentDetail author="Sam" />
-            <CommentDetail author="Jack"/>
-            <CommentDetail author="Rob"/>         
+            <ApprovalCard>
+                <h4>Warning!</h4>
+                Are you sure about this action?
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    avatar={faker.image.avatar()}
+                    author="Sam"
+                    timeAgo="Today at 4:445PM"
+                    content="Hooray!"
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    avatar={faker.image.avatar()}
+                    author="Jack"
+                    timeAgo="Today at 2:445PM" c
+                    content="Temporary" />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    avatar={faker.image.avatar()}
+                    author="Rob"
+                    timeAgo="Yesterday at 4:445PM"
+                    content="Giving a shot" />
+            </ApprovalCard>
         </div>
     );
 }
 
 ReactDOM.render(
-    <App/>,
+    <App />,
     document.querySelector('#root')
 )
